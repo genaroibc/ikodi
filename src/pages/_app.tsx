24b0005c777047bef66c28e345c23f5777beb419
genaroibc <bonavitaismael@gmail.com>
2022-12-01 19:34:07 -0300

    [FEAT]: add Poppins font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,16 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import "highlight.js/styles/github.css";
+import { Poppins } from "@next/font/google";
+
+const poppins = Poppins({
+  weight: ["400", "800"]
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={poppins.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
