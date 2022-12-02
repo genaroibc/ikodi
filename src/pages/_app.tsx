0acf3c7ba217572a9cd5fd5fa55a8fbde9dabd09
genaroibc <bonavitaismael@gmail.com>
2022-12-02 13:37:25 -0300

    [FEAT]: update Font Family

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,7 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import "highlight.js/styles/github.css";
-import { Poppins } from "@next/font/google";
-
-const poppins = Poppins({
-  weight: ["400", "800"]
-});
+import "highlight.js/styles/shades-of-purple.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <main className={poppins.className}>
-      <Component {...pageProps} />
-    </main>
-  );
+  return <Component {...pageProps} />;
 }
