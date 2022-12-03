a3885435815585be11c76edb4bc1caf712a05c45
genaroibc <bonavitaismael@gmail.com>
2022-12-03 16:42:54 -0300

    [FEAT]: add PostsCardList

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { Hero } from "../components/Hero/Hero";
+import { PostCardList } from "../components/PostCardList/PostCardList";
 import { getAllPostsMetadata } from "../lib/mdx";
 import { Post } from "../types";
 
@@ -10,6 +11,10 @@ export default function Home({ posts }: Props) {
   return (
     <>
       <Hero />
+
+      <hr />
+
+      <PostCardList posts={posts} />
     </>
   );
 }
