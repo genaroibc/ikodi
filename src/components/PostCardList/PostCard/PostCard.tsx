f60ef289caed0925cb079506ee123393ea8e7344
genaroibc <bonavitaismael@gmail.com>
2022-12-06 16:32:53 -0300

    [FEAT]: update PostCard hover

--- src/components/PostCardList/PostCard/PostCard.tsx
+++ src/components/PostCardList/PostCard/PostCard.tsx
@@ -5,30 +5,36 @@ import styles from "./PostCard.module.css";
 
 export function PostCard({ date, title, slug, reading_time }: Post) {
   return (
-    <article className={styles.post_card}>
-      <Link className={styles.post_card__link} href={`/${slug}`}>
-        Read
-      </Link>
-      <h3 className={styles.post_card__title}>{title}</h3>
-      <h4 className={styles.post_card__date}>
-        <Image
-          src="/svg/calendar-regular.svg"
-          alt="calendar icon"
-          width={20}
-          height={20}
-        />
-        {date}
-      </h4>
-      <h4 className={styles.post_card__reading_time}>
-        <Image
-          src="/svg/clock-regular.svg"
-          alt="clock icon"
-          width={20}
-          height={20}
-        />
+    <Link className={styles.link} href={`/${slug}`}>
+      <article className={styles.post_card}>
+        <h3 className={styles.post_card__title}>{title}</h3>
+        <h4 className={styles.post_card__date}>
+          <Image
+            src="/svg/calendar-regular.svg"
+            alt="calendar icon"
+            width={20}
+            height={20}
+          />
+          {date}
+        </h4>
+        <h4 className={styles.post_card__reading_time}>
+          <Image
+            src="/svg/clock-regular.svg"
+            alt="clock icon"
+            width={20}
+            height={20}
+          />
 
-        {reading_time}
-      </h4>
-    </article>
+          {reading_time}
+        </h4>
+        <Image
+          className={styles.post_card__arrow}
+          src="/svg/arrow-right.svg"
+          alt="arrow icon"
+          width={20}
+          height={20}
+        />
+      </article>
+    </Link>
   );
 }
