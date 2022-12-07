aaf6bb0788efffdb8bc6f67efe5db86bccc9851c
genaroibc <bonavitaismael@gmail.com>
2022-12-07 13:23:57 -0300

    [FEAT]: back to home link in post page

--- src/components/Article/Article.tsx
+++ src/components/Article/Article.tsx
@@ -1,12 +1,25 @@
 import { MDXRemote } from "next-mdx-remote";
+import Image from "next/image";
+import Link from "next/link";
 import { SerializedPost } from "../../types";
 import MDXComponents from "../MDXComponents/MDXComponents";
 import styles from "./Article.module.css";
 
 export function Article({ frontmatter, source }: SerializedPost) {
   return (
-    <article className={styles.article}>
-      <MDXRemote {...source} components={MDXComponents} />
-    </article>
+    <>
+      <article className={styles.article}>
+        <MDXRemote {...source} components={MDXComponents} />
+      </article>
+      <Link className={styles.back_to_home_link} href="/">
+        <Image
+          src="/svg/arrow-right.svg"
+          alt="arrow icon"
+          height={20}
+          width={20}
+        />
+        Back to home
+      </Link>
+    </>
   );
 }
