9b6f72341945442effbfca1655e5ebc1ecc34fff
genaroibc <bonavitaismael@gmail.com>
2022-12-22 15:06:30 -0300

    [FEAT]: add GET endpoint for post comments

--- src/pages/api/comments/[postId].ts
+++ src/pages/api/comments/[postId].ts
@@ -1,19 +1,50 @@
-import { deleteCommentByPostId } from "controllers/commentsController";
+import {
+  createCommentByData,
+  deleteCommentById,
+  getAllCommentsByPostId
+} from "controllers/commentsController";
 import { NextApiHandler } from "next";
 import { mongoDBConnection } from "services/mongoDBConnection";
 
 mongoDBConnection(process.env.MONGODB_DATABASE_URL);
 
 const handler: NextApiHandler = async (req, res) => {
-  const { method, body } = req;
+  const { method, body, query } = req;
 
   switch (method) {
+    case "GET":
+      try {
+        if (!query.postId) {
+          return res
+            .status(400)
+            .json({ ok: false, error_message: "PostId is required" });
+        }
+
+        const comments = await getAllCommentsByPostId(query.postId as string);
+
+        return res.status(200).json({ ok: true, comments });
+      } catch (error) {
+        // @ts-ignore
+        return res.status(error.status).json({ ok: false, ...error });
+      }
+
+    case "POST":
+      try {
+        const createdComment = await createCommentByData(body.commentData);
+
+        return res.status(200).json({ ok: true, createdComment });
+      } catch (error) {
+        // @ts-ignore
+        return res.status(error.status).json({ ok: false, ...error });
+      }
+
     case "DELETE":
       try {
-        const deletedComment = await deleteCommentByPostId(body.postId);
+        const deletedComment = await deleteCommentById(body.postId);
 
         return res.status(200).json({ ok: true, deletedComment });
       } catch (error) {
+        // @ts-ignore
         return res.status(error.status).json({ ok: false, ...error });
       }
 
