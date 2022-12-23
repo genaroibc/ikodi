d3168f0eb2042caa3d884499f922a11d513aa22d
genaroibc <bonavitaismael@gmail.com>
2022-12-23 12:34:43 -0300

    [DEV]: set mongoose strictQuery to false

--- src/services/mongoDBConnection.ts
+++ src/services/mongoDBConnection.ts
@@ -4,6 +4,8 @@ let isConnected = false;
 
 export function mongoDBConnection(uri = "") {
   if (!isConnected && uri) {
+    mongoose.set("strictQuery", false);
+
     mongoose
       .connect(uri)
       .then(() => console.log(`MongoDB connected in ${uri}!!`))
