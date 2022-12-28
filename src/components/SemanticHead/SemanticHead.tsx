31d5d2921fe291a0e827413f1c0ccb3be6cba83f
genaroibc <bonavitaismael@gmail.com>
2022-12-28 11:46:00 -0300

    [FEAT]: update head meta tags

--- src/components/SemanticHead/SemanticHead.tsx
+++ src/components/SemanticHead/SemanticHead.tsx
@@ -11,15 +11,18 @@ export function SemanticHead() {
       />
       <meta
         property="og:description"
-        content="IKodi is a blog about web development and programming were you can find articles about many different tools, technologies and web concepts."
+        content="IKodi is a blog about web development and programming, were you can find articles about many different tools, technologies and web concepts."
       />
       <meta property="og:url" content="https://ikodi.vercel.app" />
       <meta property="og:site_name" content="IKodi" />
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:title" content="IKodi - The Web Development post" />
+      <meta
+        name="twitter:title"
+        content="IKodi - Programming and Web Development"
+      />
       <meta
         name="twitter:description"
-        content="IKodi is a blog about web development and programming,  were you can find articles about many different tools, technologies and web concepts."
+        content="IKodi is a blog about web development and programming, were you can find articles about many different tools, technologies and web concepts."
       />
       <meta
         name="twitter:image"
@@ -29,7 +32,7 @@ export function SemanticHead() {
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta
         name="description"
-        content="IKodi is a blog about web development and programming were you can find articles about many different tools, technologies and web concepts."
+        content="IKodi is a blog about web development and programming, were you can find articles about many different tools, technologies and web concepts."
       />
       <base target="_blank" />
       <title>Ikodi</title>
