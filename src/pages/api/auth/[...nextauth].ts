79f8e3a3b4de7cebeb4805756d1cbde172a00db7
genaroibc <bonavitaismael@gmail.com>
2022-12-29 07:54:04 -0300

    [FIX]: add NextAuth env variables validation

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -4,7 +4,13 @@ import GitHubProvider from "next-auth/providers/github";
 
 const GITHUB_ID = process.env.GITHUB_ID;
 const GITHUB_SECRET = process.env.GITHUB_SECRET;
+const NEXTAUTH_SECRET = process.env.NEXTAUTH_SECRET;
+const NEXTAUTH_URL = process.env.NEXTAUTH_URL;
 
+if (!NEXTAUTH_SECRET)
+  throw new Error("env variable 'NEXTAUTH_SECRET' is not declared");
+if (!NEXTAUTH_URL)
+  throw new Error("env variable 'NEXTAUTH_URL' is not declared");
 if (!GITHUB_ID) throw new Error("env variable 'GITHUB_ID' is not declared");
 if (!GITHUB_SECRET)
   throw new Error("env variable 'GITHUB_SECRET' is not declared");
@@ -15,7 +21,8 @@ export const authOptions: AuthOptions = {
       clientId: GITHUB_ID,
       clientSecret: GITHUB_SECRET
     })
-  ]
+  ],
+  secret: NEXTAUTH_SECRET
 };
 
 export default NextAuth(authOptions);
